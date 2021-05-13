2c0b0bad751714eb77b6a290a02acd2b7a96abe1
Haowen Yu <howardyu77@yahoo.com>
2021-05-13 16:48:19 +0100

    finished execise 1

---
+++ Point_vortex_stream_function.m
@@ -0,0 +1,24 @@
+clear;
+close all;
+% generatematrices xm, ym, psi
+nx = 51;
+ny = 41;
+xmin = -2.5;
+xmax = 2.5
+ymin = -2.0;
+ymax = 2.0;
+xc = 0.75;
+yc = 0.5
+Gamma = 3.0
+for i=1:1:nx
+    for j=1:1:ny
+        xm(i,j) = xmin + (i-1)*(xmax-xmin)/(nx-1);
+        ym(i,j) = ymin + (j-1)*(ymax-ymin)/(ny-1);
+        psi(i,j) = psipv(xc,yc,Gamma,xm(i,j),ym(i,j));
+    end
+end
+
+
+c = -0.4:0.2:1.2;
+
+contour(xm,ym,psi,c);
