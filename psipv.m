7345eb116577e9c3f0fa05c7aa9a8352a97ac32d
Haowen Yu <howardyu77@yahoo.com>
2021-05-13 16:13:32 +0100

    computing the streamfunction

---
+++ psipv.m
@@ -0,0 +1,8 @@
+function psixy = psipv(xc,yc,Gamma,x,y)
+%Summary of this function goes here: get the streamfunction at point x,y
+%calculate the distance between x,y and xc, yc
+r=sqrt((x-xc)^2+(y-yc)^2);
+%calculate psi
+psixy=-Gamma/(4*pi)*log(r^2);
+end
+
