ad12bc4011e2ecbe6a9902afcb0a417ecb842868
Haowen Yu <howardyu77@yahoo.com>
2021-05-14 10:18:59 +0100

    sorted out ex2 step iii

--- Ex2_reference_vortex_sheet_panel_stream_function.m
+++ Ex2_reference_vortex_sheet_panel_stream_function.m
@@ -12,18 +12,26 @@ xc = 0.75;
 yc = 0.5;
 Gamma = 3.0;
 del = 1.5;
+% preallocate matrices
+xm=zeros(nx,ny);
+ym=zeros(nx,ny);
+psi=zeros(nx,ny);
+infa=zeros(nx,ny);
+infb=zeros(nx,ny);
 % generate matrices xm, ym, psi
 for i=1:1:nx
     for j=1:1:ny
         xm(i,j) = xmin + (i-1)*(xmax-xmin)/(nx-1);
         ym(i,j) = ymin + (j-1)*(ymax-ymin)/(ny-1);
         psi(i,j) = psipv(xc,yc,Gamma,xm(i,j),ym(i,j));
-        inf(i,j) = refpaninf(del,xm(i,j),ym(i,j));
+        [infa(i,j),infb(i,j)] = refpaninf(del,xm(i,j),ym(i,j));
     end
 end
 
 % draw contour plots
 c = -0.15:0.05:0.15;
 
-contour(xm,ym,inf,c);
+contour(xm,ym,infa,c);
 
+figure(2)
+contour(xm,ym,infb,c);
