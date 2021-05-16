0788e569a516d2e2c246bc89eaaeaaae5e0dbdb4
Haowen Yu <howardyu77@yahoo.com>
2021-05-16 20:08:56 +0100

    try to sort out index

--- build_lhs.m
+++ build_lhs.m
@@ -3,25 +3,21 @@ function lhsmat = build_lhs(xs,ys)
 np = length(xs) - 1; 
 psip = zeros(np,np+1);
 
-for j=1:1:np+1
-    for i=1:1:np
-        for k=1:1:np
-            %get infa and infb at pannel j
-                [infa(i,i),infb(i,i)] = panelinf(xs(j),ys(j),xs(j+1),ys(j+1),xm(i,j),ym(i,j));
-            if j==1
-                psip(i,j)=infa(i,i);
-            elseif j==np+1
-                [infa(i,i),infb(i,i)] = panelinf(xs(j-1),ys(j-1),xs(j),ys(j),xm(i,j),ym(i,j));
-                psip(i,j)=infb(i,i);
-            else
-                [infa(i,i),infb(i,i)] = panelinf(xs(j),ys(j),xs(j+1),ys(j+1),xm(i,j),ym(i,j));
-                psip(i,j)=infa(i,j)+infb(i,j);
-            end
+[infa(i,i),infb(i,i)] = panelinf(xs(j),ys(j),xs(j+1),ys(j+1),xm(i,j),ym(i,j));
+
+for ip=1:1:np+1
+    for jp=1:1:np+1
+        if jp==1
+           
+        elseif jp==np+1
+           
+           
+        else
+           
+           
         end
     end
-    
 end
-
 %initialise lhsmat
 lhsmat = zeros(np+1,np+1);
 
