a324742bfd55ddf26cf5ac6be3f6556040f638ce
Haowen Yu <howardyu77@yahoo.com>
2021-05-16 21:49:15 +0100

    added axis to ex4 plot and updated build_lhs

--- build_lhs.m
+++ build_lhs.m
@@ -3,23 +3,25 @@ function lhsmat = build_lhs(xs,ys)
 np = length(xs) - 1; 
 psip = zeros(np,np+1);
 
-[infa(i,i),infb(i,i)] = panelinf(xs(j),ys(j),xs(j+1),ys(j+1),xm(i,j),ym(i,j));
 
-for ip=1:1:np+1
+
+for ip=1:1:np
     for jp=1:1:np+1
+        [infa(ip,ip),infb(ip,ip)] = panelinf(xs(jp),ys(jp),xs(jp+1),ys(jp+1),xm(ip,ip),ym(ip,ip));
         if jp==1
-           
+           psip(ip,jp)=infa(ip,ip);
         elseif jp==np+1
-           
-           
+           psip(ip,jp)=infb(ip-1,ip-1);
         else
-           
+           psip(ip,jp)=infa(ip,ip)+infb(ip-1,ip-1);
            
         end
     end
 end
+
+
 %initialise lhsmat
 lhsmat = zeros(np+1,np+1);
-
+for jp=1:1
 end
 
