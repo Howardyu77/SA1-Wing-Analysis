006fa7c7768d3346a7ad8b8c55b14b64afc70b8e
Haowen Yu <howardyu77@yahoo.com>
2021-05-16 17:06:22 +0100

    started on build_lhs

---
+++ build_lhs.m
@@ -0,0 +1,17 @@
+function lhsmat = build_lhs(xs,ys)
+%This function assemble the matrix A
+np = length(xs) - 1; 
+psip = zeros(np,np+1);
+
+for j=1:1:np
+    for k=1:1:np
+        if j=1
+            
+        end 
+    end
+end
+%initialise lhsmat
+lhsmat = zeros(np+1,np+1);
+
+end
+
