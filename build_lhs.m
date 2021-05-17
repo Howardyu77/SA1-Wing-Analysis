2c68203c562316108be56e1ef03099df39d3e81f
Haowen Yu <howardyu77@yahoo.com>
2021-05-17 21:40:28 +0100

    updated the description for lhs, getting closer

--- build_lhs.m
+++ build_lhs.m
@@ -7,14 +7,15 @@ psip = zeros(np,np+1);
 for ip=1:1:np
     for jp=1:1:np+1
         if jp==1
-           [infa(jp,jp),infb(jp,jp)] = panelinf(xs(jp),ys(jp),xs(jp+1),ys(jp+1),xs(ip),xs(ip));
-           psip(ip,jp)=infa(jp,jp);
+           [infa(ip,jp),infb(ip,jp)] = panelinf(xs(jp),ys(jp),xs(jp+1),ys(jp+1),xs(ip),ys(ip));
+           %get infa and infb at point (x(ip),y(ip)) due to the jpth pannel
+           psip(ip,jp)=infa(ip,jp);
         elseif jp==np+1
-           [infa(jp,jp),infb(jp,jp)] = panelinf(xs(jp-1),ys(jp-1),xs(jp),ys(jp),xs(ip),xs(ip));
-           psip(ip,jp)=infb(jp-1,jp-1);
+           [infa(ip,jp),infb(ip,jp)] = panelinf(xs(jp-1),ys(jp-1),xs(jp),ys(jp),xs(ip),ys(ip));
+           psip(ip,jp)=infb(ip,jp-1);
         else
-           [infa(jp,jp),infb(jp,jp)] = panelinf(xs(jp),ys(jp),xs(jp+1),ys(jp+1),xs(ip),xs(ip));
-           psip(ip,jp)=infa(jp,jp)+infb(jp-1,jp-1);
+           [infa(ip,jp),infb(ip,jp)] = panelinf(xs(jp),ys(jp),xs(jp+1),ys(jp+1),xs(ip),ys(ip));
+           psip(ip,jp)=infa(ip,jp)+infb(ip,jp-1);
         end
     end
 end
