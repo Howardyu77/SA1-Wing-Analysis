442d429220fad03c02ca4d65123a01de2473c271
Haowen Yu <howardyu77@yahoo.com>
2021-05-17 14:24:05 +0100

    added some basic code, but there is problem with lhs function

---
+++ Ex5_panel_method_solution_for_the_cylinder_flow.m
@@ -0,0 +1,14 @@
+%define the cylinerical panels
+np=100;
+theta = (0:np)*2*pi/np;
+xs = cos(theta);
+ys = sin(theta);
+gammas = -2*sin(theta);
+
+%flow condition
+alpha=0;
+
+A = build_lhs(xs,ys);
+b = build_rhs(xs,ys,alpha); 
+gam = A\b;
+
