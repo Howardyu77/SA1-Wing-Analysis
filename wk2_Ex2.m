533863e58c0141c0e3b2e3b92d6183bb909d74a3
Haowen Yu <howardyu77@yahoo.com>
2021-05-19 15:19:43 +0100

    finished ex2 test loop

--- wk2_Ex2.m
+++ wk2_Ex2.m
@@ -6,18 +6,36 @@ ReL=50e6;
 x = linspace(0,1,101);
 n=length(x);
 %linearly varying ue/U
-ue=linspace(1,0,101);
-
-TS=zeros(n,1);
+ue=linspace(1,0.9,101);
+%find the pressure gradient 
+p_grad = gradient(ue,x);
+p_grad(1);
+TS=zeros(1,n);
 
 for i=1:1:n
-    thetasq=(0.45/ReL)*(ue(1,i))^(-6)*ueintbit(x(1,1),ue(1,1),x(1,i),ue(1,i));
-    TS(i,1)=sqrt(thetasq);
+    thetaonlsq=(0.45/ReL)*(ue(1,i))^(-6)*ueintbit(x(1,1),ue(1,1),x(1,i),ue(1,i));
+    TS(1,i)=sqrt(thetaonlsq);
 end
 
 plot(x,TS);
 hold on
-plot(x,(0.664/sqrt(ReL)).*sqrt(x));
+BS=0.664/sqrt(ReL);
+plot(x,BS.*sqrt(x));
 xlabel('x/L')
 ylabel('\theta/L')
 legend('Thwaites’ solution','Blasius solution')
+
+%test for trainsition
+for i=1:1:n
+    Rethet=ReL*ue(1,i)*TS(1,i);
+    m=-ReL*(TS(1,i)^2)*p_grad(1,i);
+    H = thwaites_lookup(m);
+    He=laminar_He(H);
+    
+    if log(Rethet) >= 18.4*He - 21.74 
+        laminar = false;
+        disp([x(i) Rethet/1000])
+    end
+end
+
+
