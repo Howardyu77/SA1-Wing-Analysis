48751386bc51f835ef709222ddf91dc35b3e7ace
Haowen Yu <howardyu77@yahoo.com>
2021-05-20 10:04:56 +0100

    so progress on ex4, but error with thickdash function

--- thickdash.m
+++ thickdash.m
@@ -1,12 +1,16 @@
 function dthickdx = thickdash(xmx0,thick)
 %UNTITLED26 Summary of this function goes here
 %   Detailed explanation goes here
-global ReL ue0 duedx
+global Re ue0 duedx %ReL, free-stream velocity, velocity gradient
 
 theta=thick(1,1);
 delta_E=thick(2,1);
-m = - theta^2/nu*duedx
-H = thwaites_lookup(m)
+
+
+ue=ue0+duedx*xmx0;
+Retheta=Re*ue/ue0*theta;%meant to be theta/L 
+
+He=delta_E/theta;
 if He>=1.46
     H=(11*He+15)/(48*He-59);
 else
@@ -16,6 +20,8 @@ c_f=0.091416*((H-1)*Retheta)^-0.232*exp(-1.26*H);
 c_diss=0.010011*((H-1)*Retheta)^(-1/6);
 
 
-
+dthickdx=zeros(2,1);
+dthickdx(1,1)=c_f/2-(H+2)/ue * duedx * theta;
+dthickdx(2,1)=c_diss-3/ue * duedx * delta_E;
 end
 
--- wk2_Ex4.m
+++ wk2_Ex4.m
@@ -1,16 +1,30 @@
 clear;
 close all;
 
-n = 101; % defines number of panels
-laminar = true; % initializes boundary layer state flag 
-ReL=1e4;
+n = 101; % defines number of panels 
+
+Re=1e4;
+ue0=1;
+duedx=0;
 x = linspace(0,1,n);
 n=length(x);
 %linearly varying ue/U
 ue=linspace(1,0.5,n);
 
 %find the velocity gradient 
-v_grad = gradient(ue,x);
-v_grad(1);
+ndduedx = gradient(ue,x);
+ndduedx(1);
 TS=zeros(1,n);
 i = 1;
+%define intital values of theta and delta_E
+x0 = 0.01;
+thick0(1) = 0.023*x0*(Re*x0)^(-1/6); 
+thick0(2) = 1.81*thick0(1);
+
+[delx, thickhist] = ode45(@thickdash,[0 0.99],thick0);
+
+theta_7=0.037*x*(Re*x)^-1/5;
+theta_9=0.023*x*(Re*x)^-1/6;
+
+Plot(x,theta_7)
+
