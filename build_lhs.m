5475c452a3eed40ee59b21478c4eaf67cb0f87c8
Naziur Kabya <mk952@cam.ac.uk>
2021-05-25 00:08:31 +0100

    lhs is now faster. My computer got hot and its messing up runtimes.

--- build_lhs.m
+++ build_lhs.m
@@ -1,26 +1,26 @@
 function lhsmat = build_lhs(xs,ys)
 %This function assemble the matrix A
 np = length(xs) - 1; 
-%build infa and infb matrices
+psip = zeros(np,np+1);
+
 infa = zeros(np,np);
 infb = zeros(np,np);
-for ip=1:1:np
-    for jp=1:1:np
+for ip=1:np
+    for jp=1:np
         [infa(ip,jp),infb(ip,jp)] = panelinf(xs(jp),ys(jp),xs(jp+1),ys(jp+1),xs(ip),ys(ip));
     end
 end
-
-%build psip from infa and infb matrices
-psip = zeros(np,np+1);
+        
+%build psi
 for ip=1:np
     psip(ip,1) = infa(ip,1);
-    psip(ip,np+1) = infb(ip,np);
     for jp=2:np
         psip(ip,jp) = infa(ip,jp)+infb(ip,jp-1);
     end
+    psip(ip,np+1) = infb(ip,np);
 end
 
-%build lhsmat from psip
+%build lhsmat
 lhsmat = zeros(np+1,np+1);
 for jp=1:1:np+1
     for ip=2:1:np
@@ -32,3 +32,10 @@ end
 %gammas
 lhsmat(1,1)=1;
 lhsmat(np+1,np+1)=1;
+
+%{
+Alternate:
+Instead of storing entire matrix of infa and infb, just store current and
+previous values. This means we do mot have to look up large matrices.
+Work on this after vectorising the code.
+%}
