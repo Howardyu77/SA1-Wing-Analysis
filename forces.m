b37ed9f68a55884209d307c916c3501633b973b1
Haowen Yu <howardyu77@yahoo.com>
2021-05-27 12:30:56 +0100

    fixed forces

--- forces.m
+++ forces.m
@@ -3,14 +3,14 @@ function [cl, cd] = forces(circ,cp,delstarl,thetal,delstaru,thetau)
 %   Detailed explanation goes here
 cl=-2*circ;
 %last points in the arrays are te
-H_te_u=delstaru(end)/thetau(end); 
-H_te_l=delstarl(end)/thetal(end);
-
+theta_te=thetal(end)+thetau(end);
+delstar_te=delstaru(end)+delstarl(end);
+H_te=delstar_te/theta_te;
 ue_te= sqrt(1-cp(end));
 
-theta_inf_u=thetau(end)*ue_te^((H_te_u+5)/2);
-theta_inf_l=thetal(end)*ue_te^((H_te_l+5)/2);
+theta_inf=theta_te*ue_te^((H_te+5)/2);
 
-cd=2*(theta_inf_u+theta_inf_l); %here chord length c is 1 and combine contributions from both surfaces
+
+cd=2*theta_inf; %here chord length c is 1 and combine contributions from both surfaces
 end
 
